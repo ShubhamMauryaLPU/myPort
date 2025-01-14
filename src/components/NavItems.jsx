a6711b7abbfa466a37efbd918c11bb48b4498b76
skmaurya08 <emailsshubham@gmail.com>
2025-01-14 11:14:03 +0530

    second

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import "./NavItems.css";
 import { Link } from 'react-router-dom';
 
-const NavItems = ({nav,idx}) => {
+const NavItems = ({nav,idx ,style}) => {
   return (
     <Link to={`/${nav.name}`}>
-        <li key={idx} className='navItems-li nav-link' >{nav.name}</li>
+        <li key={idx} className='navItems-li nav-link text-light' aria-current="page">{nav.name}</li>
     </Link>
     
   )
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -4,32 +4,55 @@ import NavItems from "../components/NavItems";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
 const NavBar = () => {
-  let [show,setShow]=useState(false);
-  let navbarButtonToggle=()=>{
+  let [show, setShow] = useState(false);
+  let navbarButtonToggle = () => {
     setShow(!show);
-  }
+  };
   return (
-    <>
-      <div className="navbar-mainDiv">
-        <Link to={"/"}>
-          <span className="navBar-main-logo ">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <Link to={"/"} style={{textDecoration:"none"}}>
+          <span className="navBar-main-logo  " >
             Shub<span style={{ color: "white" }}>ham</span>
           </span>
         </Link>
-        <ul className={show ? "navbar-mobile-button toggle":"navbar-ul"}>
-          {navData.map((data, idx) => (
-            <NavItems nav={data} key={idx} />
-          ))}
-          <Link to={"/contact"}>
-            <button className="navbar-contact-button mt-2">Contact Me</button>
-          </Link>
-        </ul>
-        <button className="navbar-mobile-button" onClick={navbarButtonToggle}>
-          <ion-icon name="menu-outline" className="navbar-toggle-icon"></ion-icon>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon text-light fs-2"><ion-icon name="menu-outline"className="navbar-menu-icon" ></ion-icon></span>
         </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+          <div class="navbar-nav ">
+              {navData.map((data, idx) => (
+                <NavItems nav={data} key={idx}/>
+              ))}
+              <Link to={"/contact"} style={{textDecoration:"none"}}>
+                <button className="navbar-contact-button mt-2 nav-link">
+                  Contact Me
+                </button>
+              </Link>
+          </div>
+        </div>
       </div>
-    </>
+    </nav>
   );
 };
 
 export default NavBar;
+{
+  /* <>
+      <div className="navbar-mainDiv">
+        
+        
+        <button className="navbar-mobile-button" onClick={navbarButtonToggle}>
+          <ion-icon name="menu-outline" className="navbar-toggle-icon"></ion-icon>
+        </button>
+      </div>
+    </> */
+}
